e4c61cc65881bf43820b0699f2af314da01eb35f
Hernan Damico <Hernii.3@hotmail.com>
2021-02-01 18:12:01 -0300

    Commit message

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import NavbarComponent from './components/Navbar/';
+
+
+function App() {
+   return (
+    <>
+    <NavbarComponent/>
+      
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,18 @@
+
+
+const NavbarComponent = () => {
+   return (
+    <>
+        <ul/> Productos
+        <li> Mate </li>
+        <li> Tablas </li>
+        <li> Cuchillos </li>
+        <li> Packs </li>
+
+
+    </>
+
+  );
+}
+
+export default NavbarComponent;
